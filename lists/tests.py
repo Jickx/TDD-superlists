dcb89f5bc4dae3dedd3fe2e7926c2017844a2101
Jickx <valentin.sharonov@gmail.com>
2024-02-08 18:06:51 +0300

    url config, route '' to homepage view

--- lists/tests.py
+++ lists/tests.py
@@ -3,11 +3,9 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 
-class TestHomePage(TestCase):
-    def test_home_page_valid(self):
-        response = home_page(HttpRequest())
-        html = response.content.decode('utf8')
-
-        self.assertIn('<title>To-Do list</title>', html)
-        self.assertTrue(html.startswith('<html>'))
-        self.assertTrue(html.endswith('</html>'))
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, '<title>To-Do list</title>')
+        self.assertContains(response, '<html>')
+        self.assertContains(response, '</html>')
